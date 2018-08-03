0f28c1ee673010c6cc839ae4c5e21639f876cd08
AdamT213 <adt6261@gmail.com>
2018-08-03 16:31:53 -0400

    added css animations to app header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,13 +6,15 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title-top">Hi Adam, welcome to your Personal Poker Universe</h1> 
+          <h2 className="App-title-middle">Not Adam? GTFO!</h2> 
+          <h2 className="App-title-bottom">Just Kidding, you can stay</h2>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+
     );
   }
 }
