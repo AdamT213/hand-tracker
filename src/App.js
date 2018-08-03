b459db177862e659570d5b52a55f23ae80ca2a93
AdamT213 <adt6261@gmail.com>
2018-08-03 16:58:24 -0400

    stubbed out basic homepage with h4s in the place of links to come; continued developing aesthetic

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.css'; 
+import Home from './Components/Home'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title-top">Hi Adam, welcome to your Personal Poker Universe</h1> 
-          <h2 className="App-title-middle">Not Adam? GTFO!</h2> 
-          <h2 className="App-title-bottom">Just Kidding, you can stay</h2>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h1 className="App-title-top">Hi Adam, welcome to your Personal Poker Universe!</h1> 
+          <h1 className="App-title-middle">Not Adam? GTFO!</h1> 
+          <h1 className="App-title-bottom">Just Kidding, you can stay</h1>
+        </header> 
+        <Home />
       </div>
-
     );
   }
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+export class Home extends Component { 
+     
+  render() { 
+      return ( 
+        <div> 
+          <h4> Add a new hand </h4> 
+          <h4> Search For Hands By Category </h4> 
+          <h4> Search For Sessions By Category </h4> 
+          <h4> Search For Tables By Category </h4>
+        </div> 
+      ); 
+  }
+} 
+
+export default Home; 
