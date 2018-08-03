3d58396a284839e9f7811f579f62b56e4a6cb1ee
AdamT213 <adt6261@gmail.com>
2018-08-03 17:52:54 -0400

    stubbed out form to add hand and link to it from homepage

---
+++ src/Components/Forms/addHand.js
@@ -0,0 +1,266 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'; 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";
+import { saveHand } from '../Actions/handActions'; 
+
+export class addHand extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = { 
+      position: '',
+      preFlopRaise: '',
+      flopBet: '',
+      turnBet: '',
+      riverBet: '',
+      playersToFlop: '', 
+      playersToTurn: '',
+      playersToRiver: '',
+      playersToShowdown: '', 
+      status: '',
+      potSize: '',
+      holeCards: '',
+      flop: '',
+      turn: '',
+      river: ''
+    }
+  }
+
+  handleOnChange = event => {
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleOnSubmit = event => { 
+    const hand = Object.assign({}, this.state); 
+    event.preventDefault();
+    this.props.addHand(hand);   
+    this.setState({
+      position: '',
+      preFlopRaise: '',
+      flopBet: '',
+      turnBet: '',
+      riverBet: '',
+      playersToFlop: '', 
+      playersToTurn: '',
+      playersToRiver: '',
+      playersToShowdown: '', 
+      status: '',
+      potSize: '',
+      holeCards: '',
+      flop: '',
+      turn: '',
+      river: ''
+    });
+  }
+
+  render() { 
+
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 col-md-offset-2">
+              <div className="panel panel-default">
+                <div className="panel-body">
+                  <form className="form-horizontal" onSubmit={this.handleOnSubmit}>
+                  <div className="form-group">
+                      <label htmlFor="holeCards" className="col-md-4 control-label">What were your hole cards?</label>
+                      <div className="col-md-5">
+                        <input
+                          className="form-control"
+                          type="text"
+                          name="holeCards"
+                          value={this.state.holeCards}
+                          onChange={this.handleOnChange}
+                        />
+                      </div>
+                    </div> 
+                    <div className="form-group">
+                      <label htmlFor="position" className="col-md-4 control-label">Position</label>
+                      <div className="col-md-5">
+                        <input
+                          className="form-control"
+                          name="position"
+                          value={this.state.position}
+                          onChange={this.handleOnChange}
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="flop" className="col-md-4 control-label">What was the flop?</label>
+                      <div className="col-md-5">
+                        <input
+                          className="form-control"
+                          type="text"
+                          name="flop"
+                          value={this.state.flop}
+                          onChange={this.handleOnChange}
+                        />
+                      </div>
+                    </div>  
+                    <div className="form-group">
+                      <label htmlFor="turn" className="col-md-4 control-label">What was the turn?</label>
+                      <div className="col-md-5">
+                        <input
+                          className="form-control"
+                          type="text"
+                          name="turn"
+                          value={this.state.turn}
+                          onChange={this.handleOnChange}
+                        />
+                      </div>
+                    </div>  
+                    <div className="form-group">
+                      <label htmlFor="river" className="col-md-4 control-label">What was the river?</label>
+                      <div className="col-md-5">
+                        <input
+                          className="form-control"
+                          type="text"
+                          name="river"
+                          value={this.state.river}
+                          onChange={this.handleOnChange}
+                        />
+                      </div>
+                    </div>  
+                    <div className="form-group">
+                      <label htmlFor="preFlopRaise" className="col-md-4 control-label">Preflop Raise</label>
+                      <div className="col-md-5">
+                        <textarea
+                          className="form-control"
+                          name="preFlopRaise"
+                          value={this.state.preFlopRaise}
+                          onChange={this.handleOnChange}
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="flopBet" className="col-md-4 control-label">Flop Bet</label>
+                      <div className="col-md-5">
+                        <textarea
+                          className="form-control"
+                          type="text"
+                          name="flopBet"
+                          value={this.state.flopBet}
+                          onChange={this.handleOnChange}
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="turnBet" className="col-md-4 control-label">Turn Bet</label>
+                      <div className="col-md-5">
+                        <textarea
+                          className="form-control"
+                          type="text"
+                          name="turnBet"
+                          value={this.state.turnBet}
+                          onChange={this.handleOnChange}
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="riverBet" className="col-md-4 control-label">River Bet</label>
+                      <div className="col-md-5">
+                        <textarea
+                          className="form-control"
+                          type="text"
+                          name="riverBet"
+                          value={this.state.riverBet}
+                          onChange={this.handleOnChange}
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="playersToFlop" className="col-md-4 control-label">How many players saw the flop?</label>
+                      <div className="col-md-5">
+                        <input
+                          className="form-control"
+                          type="text"
+                          name="playersToFlop"
+                          value={this.state.playersToFlop}
+                          onChange={this.handleOnChange}
+                        />
+                      </div>
+                    </div> 
+                    <div className="form-group">
+                      <label htmlFor="playersToTurn" className="col-md-4 control-label">How many players saw the turn?</label>
+                      <div className="col-md-5">
+                        <input
+                          className="form-control"
+                          type="text"
+                          name="playersToTurn"
+                          value={this.state.playersToTurn}
+                          onChange={this.handleOnChange}
+                        />
+                      </div>
+                    </div> 
+                    <div className="form-group">
+                      <label htmlFor="playersToRiver" className="col-md-4 control-label">How many players saw the river?</label>
+                      <div className="col-md-5">
+                        <input
+                          className="form-control"
+                          type="text"
+                          name="playersToRiver"
+                          value={this.state.playersToRiver}
+                          onChange={this.handleOnChange}
+                        />
+                      </div>
+                    </div>  
+                    <div className="form-group">
+                      <label htmlFor="playersToShowdown" className="col-md-4 control-label">How many players went to a showdown?</label>
+                      <div className="col-md-5">
+                        <input
+                          className="form-control"
+                          type="text"
+                          name="playersToShowdown"
+                          value={this.state.playersToShowdown}
+                          onChange={this.handleOnChange}
+                        />
+                      </div>
+                    </div> 
+                    <div className="form-group">
+                      <label htmlFor="status" className="col-md-4 control-label">Did you win the hand(Y/N)</label>
+                      <div className="col-md-5">
+                        <input
+                          className="form-control"
+                          type="text"
+                          name="status"
+                          value={this.state.status}
+                          onChange={this.handleOnChange}
+                        />
+                      </div>
+                    </div> 
+                    <div className="form-group">
+                      <label htmlFor="potSize" className="col-md-4 control-label">How big was the pot?</label>
+                      <div className="col-md-5">
+                        <input
+                          className="form-control"
+                          type="text"
+                          name="potSize"
+                          value={this.state.potSize}
+                          onChange={this.handleOnChange}
+                        />
+                      </div>
+                    </div> 
+                    <div className="form-group">
+                      <div className="col-md-6 col-md-offset-4">
+                        <button type="submit" className="btn btn-default">Good Luck!</button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+      ); 
+    }
+  }
+export default connect(null, { saveHand })(addHand);
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom"; 
+import addHand from './Forms/addHand'
 
 export class Home extends Component { 
      
   render() { 
       return ( 
         <div> 
-          <h4> Add a new hand </h4> 
+          <Link to={ addHand }> Add a new hand </Link> 
           <h4> Search For Hands By Category </h4> 
           <h4> Search For Sessions By Category </h4> 
           <h4> Search For Tables By Category </h4>
