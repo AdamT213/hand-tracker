decd7538217345aa00637af4343f2b31ee3eae08
Adam <adt6261@gmail.com>
2019-03-06 19:12:37 -0500

    got header to resize after animation

--- src/App.js
+++ src/App.js
@@ -1,44 +1,88 @@
-import React, { Component } from 'react';
-import './App.css'; 
-import { Router, Route, Switch } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
-import Home from './Components/Home'
-import Navbar from './Components/Navbar';
-import showSession from './Components/showSession'; 
-import showTable from './Components/showTable';
-import showHand from './Components/showHand';
-import sessionsList from './Components/sessionsList';
+import React, { Component } from "react";
+import "./App.css"; 
+import { Router, Route, Switch } from "react-router-dom";
+import createBrowserHistory from "history/createBrowserHistory";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import showSession from "./Components/showSession"; 
+import showTable from "./Components/showTable";
+import showHand from "./Components/showHand";
+import sessionsList from "./Components/sessionsList";
+import styles from "styled-components";
 
 export const history = createBrowserHistory();
 
+const FadingHeader = styles.header`
+  background-color: black;
+  color: #7fff00;
+  height: ${props => props.animation ? "150px" : "75px"}
+  padding: ${props => props.animation ? "20px" : "7px"}
+`;
+
+const H1FadeInLeft = styles.h1`
+  font-size: 1.5em;
+  animation-duration: 3s;
+  animation-name: slidein-right;
+`;
+
+const H1FadeInRightDisappear = styles.h1`
+  font-size: 1.5em; 
+  animation-delay: 3s;
+  animation-duration: 3s;
+  visibility: hidden;
+  animation-name: slidein-left;
+`;
+
+const H1FadeInLeftDisappear = styles.h1`
+  font-size: 1.5em; 
+  animation-delay: 5s;
+  animation-duration: 4s;
+  visibility: hidden;
+  animation-name: slidein-right;
+`;
+
 class App extends Component {
+	constructor(){ 
+		super();
+		this.state={
+			animation: true,
+		};
+	}
+  
+	componentDidMount(){
+		setTimeout(() => {
+			return this.setState({
+				animation: false
+			});
+		}, 10000);
+	}
 
-  render() {
+	render(){
 
-    document.title = "Wilkommen";
+		document.title = "Wilkommen";
     
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title-top">Hi Adam, welcome to your Personal Poker Universe!</h1> 
-          <h1 className="App-title-middle">Not Adam? GTFO!</h1> 
-          <h1 className="App-title-bottom">Just Kidding, you can stay</h1>
-        </header> 
-        <Router history= {history}> 
-          <Switch>
-          <div>
-          <Navbar/>
-          <Route exact path= "/" component= {Home} />  
-          <Route exact path= "/session/:id" component= {showSession} /> 
-          <Route path= "/sessions/index" component= {sessionsList} /> 
-          <Route exact path= "/session/:id/table/:id" component= {showTable}/> 
-          <Route path= "/session/:id/table/:id/hand/:id" component= {showHand} />
-          </div>
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
+		return (
+			<div className="App">
+				<FadingHeader animation={this.state.animation}>
+					<H1FadeInLeft>Hi Adam, welcome to your Personal Poker Universe!</H1FadeInLeft>
+					<H1FadeInRightDisappear className="App-title-middle">Not Adam? GTFO!</H1FadeInRightDisappear>
+					<H1FadeInLeftDisappear>Just Kidding, you can stay</H1FadeInLeftDisappear>
+				</FadingHeader>
+				<Router history= {history}>
+					<Switch>
+						<div>
+							<Navbar/>
+							<Route exact path= "/" component= {Home} />  
+							<Route exact path= "/session/:id" component= {showSession} /> 
+							<Route path= "/sessions/index" component= {sessionsList} /> 
+							<Route exact path= "/session/:id/table/:id" component= {showTable}/> 
+							<Route path= "/session/:id/table/:id/hand/:id" component= {showHand} />
+						</div>
+					</Switch>
+				</Router>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; 
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk';
-import './index.css';
-import rootReducer from './Reducers';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom"; 
+import {Provider} from "react-redux";
+import {createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import "./index.css";
+import rootReducer from "./Reducers";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
 export const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.devToolsExtension ? window.devToolsExtension() : f => f
-  )
-)
+	rootReducer,
+	compose(
+		applyMiddleware(thunk),
+		window.devToolsExtension ? window.devToolsExtension() : f => f
+	)
+);
 
 
 ReactDOM.render(
-<Provider store={store}> 
-    <App />  
-</Provider>,
-document.getElementById('root'));
+	<Provider store={store}> 
+		<App /> 
+	</Provider>,
+	document.getElementById("root"));
 
 registerServiceWorker();
